1b16371634fe6cf4ea88096d4e04ad0d304d4925
daw2-herrera21 <71894765+daw2-herrera21@users.noreply.github.com>
2020-11-03 00:02:38 +0100

    Create ej6.php

---
+++ ej6.php
@@ -0,0 +1,23 @@
+<html>
+
+    <h1>Lista 10 cosas importantes aprendidas:</h1>
+
+    <p>Crear claves foraneas<br></p>
+    <p>Hacer inserts con mysql y php<br></p>
+    <p>Crear un bucle para realizar una paginacion de un documento<br></p>
+    <p>Extraer datos de base de datos con select<br></p>
+    <p>Crear documentos con etiquetas html extrayendo datos de la base de datos<br></p>
+    <p>Generar el resultado entre dos tablas diferentes dentro de la misma base de datos<br></p>
+    <p>No se me ocurre nada mas.<br></p>
+    <p>No se me ocurre nada mas.<br></p>
+    <p>No se me ocurre nada mas.<br></p>
+    <p>No se me ocurre nada mas.<br></p>
+
+
+    <h1>Documento y explicación del professor:</h1>
+    <p>Documento=5; Explicación del professor=6.<br></p>
+    <h1>Autoevaluacion:</h1>
+    <p>Un 6.<br></p>
+    <h1>Mejoras:</h1>
+    <p>Nada a comentar.<br></p>    
+</html>
